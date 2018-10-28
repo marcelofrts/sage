cbaf2b0c9c169d3fefcb8bd5f68f778ea8546625
Marcelo Freitas <marcelofrts@hotmail.com>
2018-10-28 16:34:22 -0300

    Adicionar arquivos de projeto.

---
+++ SAGE-APP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAGE_APP.MvcApplication" Language="C#" %>
---
+++ SAGE-APP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAGE_APP.Startup))]
+namespace SAGE_APP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
